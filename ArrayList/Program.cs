0bc6f01e5e09c27b5e6ae385f585e626ab476ad5
shreeshams123 <[shreesha2002ms@gmail.com]>
2024-09-17 11:16:52 +0530

    ADDED Arralist Project

---
+++ ArrayList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+namespace ArrayListsExample
+
+{
+   class Program
+    {
+        public static void Main(string[] args)
+        {
+           ArrayList a = new ArrayList();
+            a.Add(1);
+            a.Add("John");
+            a.AddRange(new ArrayList() { 2, 5, "Rachael", 7.2 });
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine(a.Capacity);
+            Console.WriteLine(a.Count);
+            a.Reverse();
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+            a.Clear();
+            Console.WriteLine(a.Count);
+        }
+    }
+}
